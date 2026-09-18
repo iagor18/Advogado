7c23a21ea9429bc2d3e62e894d4452f44b683f9a
Iago Rayner <iagorduarte18@gmail.com>
2026-09-18 11:27:56 -0300

    Lower case

--- Js/script.js
+++ Js/script.js
@@ -52,8 +52,7 @@ document.addEventListener("DOMContentLoaded", () => {
         },
 
     };
-
-
+    
     // =====================================================
     // DESCOBRE QUAL CLIENTE FOI SELECIONADO PELA URL
     //https://iagor18.github.io/Advogado/?cliente=sergio
@@ -65,11 +64,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     const clienteSelecionado =
-        parametros.get("cliente");
-
+    parametros.get("cliente")?.toLowerCase();
 
     const cliente =
-        clientes[clienteSelecionado] || clientes.padrao;
+    clientes[clienteSelecionado] || clientes.padrao;
 
 
     console.log("Cliente selecionado:", clienteSelecionado);
